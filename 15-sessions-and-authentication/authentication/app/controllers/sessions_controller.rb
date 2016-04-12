f583142bebd403053d7dcb5d57e65ae41ae44e09
Fred Ngo <fredngo@gmail.com>
2016-04-12 00:05:59 -0400

    Implement SessionsController and new session view.

--- 15-sessions-and-authentication/authentication/app/controllers/sessions_controller.rb
+++ 15-sessions-and-authentication/authentication/app/controllers/sessions_controller.rb
@@ -1,10 +1,25 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
+  # Logging in
   def create
+    user = User.find_by email: params[:email]
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: 'Successfully logged in!'
+    else
+      flash[:alert] = 'Invalid credentials!'
+      render :new
+    end
   end
 
+  # Logging out
   def destroy
+    session[:user_id] = nil
+    redirect_to root_url
   end
+
 end
