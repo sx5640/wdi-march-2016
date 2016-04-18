a232b0b746e38ab5ba86f340f55abbf30d143ea0
Fred Ngo <fredngo@gmail.com>
2016-04-18 00:22:33 -0400

    Add second simple Restaurant test

--- 19-testing-with-rails/seatyourself/test/models/restaurant_test.rb
+++ 19-testing-with-rails/seatyourself/test/models/restaurant_test.rb
@@ -4,4 +4,8 @@ class RestaurantTest < ActiveSupport::TestCase
   test 'restaurants exist' do
     assert_equal 2, Restaurant.all.size
   end
+
+  test 'small restaurant has small capacity' do
+    assert restaurants(:small_restaurant).capacity < 25
+  end
 end
