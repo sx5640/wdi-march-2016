fd36aaf3d1012a6743e28eb476c0e35cbea436ef
Fred Ngo <fredngo@gmail.com>
2016-04-18 03:05:27 -0400

    Refactor available_capacity into a private method

--- 19-testing-with-rails/seatyourself/app/models/restaurant.rb
+++ 19-testing-with-rails/seatyourself/app/models/restaurant.rb
@@ -3,12 +3,17 @@ class Restaurant < ActiveRecord::Base
   has_many :reservations
 
   def available?(party_size, time)
-    available_capacity = capacity - reservations.where(time: time.beginning_of_hour..time.end_of_hour).sum(:party_size)
+    party_size > 0 && party_size <= available_capacity(time)
+  end
+
+  private
+
+  def available_capacity(time)
+    capacity - reservations.where(time: time.beginning_of_hour..time.end_of_hour).sum(:party_size)
 
     # Alternative expression of the time range, also works
     #available_capacity = capacity - reservations.where('time > ? and time < ?', time.beginning_of_hour, time.end_of_hour).sum(:party_size)
 
-    party_size > 0 && party_size <= available_capacity
   end
 
 end
