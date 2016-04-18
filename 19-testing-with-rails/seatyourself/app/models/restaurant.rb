bb6d7e10d724414c83968606b111d0bb15c3004d
Fred Ngo <fredngo@gmail.com>
2016-04-18 03:01:38 -0400

    Restaurant.available? method now takes time into account

--- 19-testing-with-rails/seatyourself/app/models/restaurant.rb
+++ 19-testing-with-rails/seatyourself/app/models/restaurant.rb
@@ -2,8 +2,11 @@ class Restaurant < ActiveRecord::Base
 
   has_many :reservations
 
-  def available?(party_size)
-    available_capacity = capacity - reservations.sum(:party_size)
+  def available?(party_size, time)
+    available_capacity = capacity - reservations.where(time: time.beginning_of_hour..time.end_of_hour).sum(:party_size)
+
+    # Alternative expression of the time range, also works
+    #available_capacity = capacity - reservations.where('time > ? and time < ?', time.beginning_of_hour, time.end_of_hour).sum(:party_size)
 
     party_size > 0 && party_size <= available_capacity
   end
--- 19-testing-with-rails/seatyourself/test/models/restaurant_test.rb
+++ 19-testing-with-rails/seatyourself/test/models/restaurant_test.rb
@@ -4,28 +4,36 @@ class RestaurantTest < ActiveSupport::TestCase
 
   setup do
     @restaurant = FactoryGirl.create :restaurant
+    @time       = Time.now
   end
 
   test 'restaurant is available' do
-    assert @restaurant.available?(10)
+    assert @restaurant.available?(10, @time)
   end
 
   test 'restaurant is not available' do
-    assert_not @restaurant.available?(200)
+    assert_not @restaurant.available?(200, @time)
   end
 
   test 'restaurant is not available for a party size of 0' do
-    assert_not @restaurant.available?(0)
+    assert_not @restaurant.available?(0, @time)
   end
 
   test 'restaurant is available given a reservation' do
     FactoryGirl.create :reservation, party_size: 10, restaurant: @restaurant
-    assert @restaurant.available?(80)
+    assert @restaurant.available?(80, @time)
   end
 
   test 'restaurant is not available given a reservation' do
     FactoryGirl.create :reservation, party_size: 99, restaurant: @restaurant
-    assert_not @restaurant.available?(10)
+    assert_not @restaurant.available?(10, @time)
+  end
+
+  test 'restaurant is available with reservations at different times' do
+    FactoryGirl.create :reservation, party_size: 99, restaurant: @restaurant, time: @time
+
+    next_time = @time + 1.hour
+    assert @restaurant.available?(10, next_time)
   end
 
 end
