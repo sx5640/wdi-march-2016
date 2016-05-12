19ee6549f5de3320a158853ed1b25f3e0156867a
Fred Ngo <fredngo@gmail.com>
2016-05-12 01:17:23 -0400

    Add marker

--- 38-apis-config-management-oauth/seatyourself/app/assets/javascripts/maps.js
+++ 38-apis-config-management-oauth/seatyourself/app/assets/javascripts/maps.js
@@ -1,10 +1,18 @@
 function initMap() {
 
-  mapPlaceholder = $('#map');
+  var mapPlaceholder = $('#map');
 
-  map = new google.maps.Map(mapPlaceholder[0], {
-    center: {lat: mapPlaceholder.data('latitude'), lng: mapPlaceholder.data('longitude')},
+  var latLong = { lat: mapPlaceholder.data('latitude'), lng: mapPlaceholder.data('longitude') }
+
+  var map = new google.maps.Map(mapPlaceholder[0], {
+    center: latLong,
     zoom: 16
+  });
+
+  var marker = new google.maps.Marker({
+    position: latLong,
+    map: map,
+    title: 'Hello World!'
   });
 
 }
