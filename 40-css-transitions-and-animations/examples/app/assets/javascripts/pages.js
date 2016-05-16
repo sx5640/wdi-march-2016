bd910f9d81ac4cbc18564b4f7217b54cc7af7694
Mina Mikhail <mina@fightingtheboss.com>
2016-05-16 13:04:43 -0400

    Add in-class code for w9d1 CSS Transitions and Animations

--- 40-css-transitions-and-animations/examples/app/assets/javascripts/pages.js
+++ 40-css-transitions-and-animations/examples/app/assets/javascripts/pages.js
@@ -1,2 +1,20 @@
 $(document).on('ready page:load', function() {
+  $('.shape').on('click', function() {
+    $(this).toggleClass('is-active');
+
+    // $(this).css({
+    //   left: '80%',
+    //   top: '50%'
+    // });
+  });
+
+
+  // Hamburger
+  $('#nav-toggle').on('click', function(e) {
+    e.preventDefault();
+
+    $(this).add('.sidebar').toggleClass('is-open');
+    // $('.sidebar').toggleClass('is-open');
+  })
+
 });
